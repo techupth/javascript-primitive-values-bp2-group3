d5e2f8682a066b7647180f1fcd7f14342becaf99
planohave <planohavev2@gmail.com>
2023-12-02 11:06:50 +0700

    feat:homework

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let originalPrice = 27000;
+let discountPrice = originalPrice/25*100;
+let  realPrice= discountPrice;
+console.log (realPrice);
